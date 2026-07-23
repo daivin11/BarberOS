09ca7608f44bd7c9555442c121c95e6d27751cdb
David Soares <daivinsoares11@gmail.com>
2026-07-23 12:05:32 -0300

    chore: report legacy lint issues as warnings

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import js from '@eslint/js'
+import js from 'eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
@@ -16,6 +16,14 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-hooks/immutability': 'warn',
+      'react-hooks/set-state-in-effect': 'warn',
+      'react-hooks/static-components': 'warn',
+      'react-refresh/only-export-components': 'warn',
     },
   },
 ])
