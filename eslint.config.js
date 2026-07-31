7eaf2c6bd2fedce8a6fba62262278b1d8cb11a74
daivin11 <daivinsoares11@gmail.com>
2026-07-31 20:38:54 -0300

    Quiet noisy React hooks lint rule

--- eslint.config.js
+++ eslint.config.js
@@ -22,7 +22,7 @@ export default defineConfig([
       'no-unused-vars': 'warn',
       'react-hooks/exhaustive-deps': 'warn',
       'react-hooks/immutability': 'warn',
-      'react-hooks/set-state-in-effect': 'warn',
+      'react-hooks/set-state-in-effect': 'off',
       'react-hooks/static-components': 'warn',
       'react-refresh/only-export-components': 'warn',
     },
